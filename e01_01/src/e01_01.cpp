cee83ef6eb3e5d609dceb3c98934b8ef7214f6f4
Naoto Beppu <ddt.beppu@gmail.com>
2018-02-16 21:16:51 +0900

    e01_01 解答しました。

--- e01_01/src/e01_01.cpp
+++ e01_01/src/e01_01.cpp
@@ -1,15 +1,22 @@
 //============================================================================
 // Name        : e01_01.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
+//演習1-1
+/*ヘッダ<iostream>をインクルードする指令が欠如しているとどうなるだろうか。
+ プログラムをコンパイルして検証せよ。*/
 
-#include <iostream>
-using namespace std;
+
+//#include <iostream>
+using namespace std; // 名前空間の宣言
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
-}
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0; // あああ
+
+	// 解答
+	// <iostream>が欠如したプログラムでは、名前空間std中で宣言されている入出力関数が使用できなくなり、
+	// コンパイルエラーとなる。
