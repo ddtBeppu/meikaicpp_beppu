6c770ca2a9847e36db3b5b50d6c022c9e2451947
Naoto Beppu <ddt.naoto@gmail.com>
2018-02-17 22:17:40 +0900

    e01_04 解答しました。

---
+++ e01_04/src/e01_04.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : e01_04.cpp
+// Author      : Naoto Beppu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// 問題文
+// 1行に1文字ずつ名前を表示するプログラムを作成せよ。
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ 		cout << "別" << endl; //文字を表示したのち、改行
+ 		cout << "府" << endl; //文字を表示したのち、改行
+ 		cout << "直" << endl; //文字を表示したのち、改行
+ 		cout << "人" << endl; //文字を表示したのち、改行
+
+	return 0; //整数を返す
+}
