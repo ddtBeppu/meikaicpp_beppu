f628ef451dcbb1646ccf8faf8eb21a68531db1c7
Naoto Beppu <ddt.naoto@gmail.com>
2018-02-17 22:15:29 +0900

    e01_02  バックスラッシュを修正しました。

--- e01_02/src/e01_02.cpp
+++ e01_02/src/e01_02.cpp
@@ -3,7 +3,7 @@
 // Author      : Naoto Beppu
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Ansi-style
 //============================================================================
 
 // 問題文
@@ -11,11 +11,11 @@
 // coutをstd::coutに変更したプログラムを作成せよ。
 
 #include <iostream>
-//using namespace std;
+using namespace std;
 
 int main() {
-	std::cout << "初めてのC++プログラム。¥n"; // テキストの出力
-	std::cout << "画面に出力しています。¥n"; // テキストの出力
+	std::cout << "初めてのC++プログラム。\n"; // テキストの出力
+	std::cout << "画面に出力しています。\n"; // テキストの出力
 
 	return 0;
 }
