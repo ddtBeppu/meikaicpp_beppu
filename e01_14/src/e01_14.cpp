bf5c45570f818e71396b38efb08840648604126e
Naoto Beppu <ddt.naoto@gmail.com>
2018-02-18 12:31:40 +0900

    e01_14 解答しました。

---
+++ e01_14/src/e01_14.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : e01_14.cpp
+// Author      : Naoto Beppu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// 問題文
+// キーボードから読み込んだ整数値プラスマイナス5の範囲の整数値を
+//  ランダムに生成して表示するプログラムを作成せよ。
+// ※　キーボードから読み込んだ値が100であれば、95 ~ 105の範囲の整数値を表示すること
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	int txt; // 入力値を格納する変数
+
+	cout << "整数値: "; // テキストの出力
+	cin >> txt; // キーボードからの入力
+
+	srand(time(NULL)); // 乱数の種を生成
+	int num = rand() % 10 - 5; // -5 ~ 5の乱数を生成
+	int output = txt + num; // 入力値に加算
+
+	// 結果の出力
+	cout << "出力値: " << output <<endl;
+
+	return 0; // 整数値を返す
+}
