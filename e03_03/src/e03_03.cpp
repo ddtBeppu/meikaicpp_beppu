3bd0fec90cfb6169e3626eb76259f4cfaabfa96d
Naoto Beppu <ddt.naoto@gmail.com>
2018-02-28 19:16:38 +0900

    e03_03 解答しました。

---
+++ e03_03/src/e03_03.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : e03_03.cpp
+// Author      : Naoto Beppu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+// 演習3-3
+// 二つの整数値を読み込んで、小さい方の数以上で大きい方の数以下の全整数を、
+// 小さい方から順に表示するプログラムを作成せよ。
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int int_a, int_b; // 二つの整数を格納する変数を宣言
+	int min, max; // 最小値、最大値を格納する変数を宣言
+
+	cout << "整数a: "; // テキストを出力
+	cin >> int_a; // キーボードからの入力
+
+	cout << "整数b: "; // テキストを出力
+	cin >> int_b; // キーボードからの入力
+
+	if (int_a < int_b) // 整数aが整数bより小さい時
+		min = int_a, max = int_b; // 最大値：整数a、最小値：整数b
+	else // 整数aが整数bより大きい時
+		min = int_b, max = int_a; // 最小値：整数a、最大値：整数b
+
+	// 最小値を出力
+	cout << min << " ";
+	do{ // 最小値が最大値と等しくない間、以下を実行
+		min ++; // 最小値に１を加算
+		// 数値を出力
+		cout << min << " ";
+	} while (min != max); // 最小値が最大値と等しくない場合、処理を続行
+
+	return 0; // 整数値を返す
+}
