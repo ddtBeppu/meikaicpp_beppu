b168122450164e838614c309be015721187f2d51
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-02 20:14:42 +0900

    e03_03 修正しました。

--- e03_03/src/e03_03.cpp
+++ e03_03/src/e03_03.cpp
@@ -14,24 +14,32 @@
 using namespace std;
 
 int main() {
-	int int_a, int_b; // 二つの整数を格納する変数を宣言
-	int min, max; // 最小値、最大値を格納する変数を宣言
+	int int_a = 0; // 任意の整数を格納
+	int int_b = 0; // 任意の整数を格納
+	int min = 0; // 最小値を格納する変数
+	int max = 0; // 最大値を格納する変数
 
-	cout << "整数a: "; // テキストを出力
-	cin >> int_a; // キーボードからの入力
+	// 値の入力を促すコメント
+	cout << "整数a: ";
+	cin >> int_a; // キーボードから値を読み込み
 
-	cout << "整数b: "; // テキストを出力
-	cin >> int_b; // キーボードからの入力
+	// 値の入力を促すコメント
+	cout << "整数b: ";
+	cin >> int_b; // キーボードから値を読み込み
 
-	if (int_a < int_b) // 整数aが整数bより小さい時
-		min = int_a, max = int_b; // 最大値：整数a、最小値：整数b
-	else // 整数aが整数bより大きい時
-		min = int_b, max = int_a; // 最小値：整数a、最大値：整数b
+	if (int_a < int_b) {// 整数aが整数bより小さい時
+		min = int_a; // 最小値：整数a
+		max = int_b; // 最大値：整数b
+	} else {// 整数aが整数bより大きい時
+		min = int_b; // 最小値：整数a
+		max = int_a; // 最大値：整数b
+	}
 
 	// 最小値を出力
 	cout << min << " ";
 	do{ // 最小値が最大値と等しくない間、以下を実行
 		min ++; // 最小値に１を加算
+
 		// 数値を出力
 		cout << min << " ";
 	} while (min != max); // 最小値が最大値と等しくない場合、処理を続行
