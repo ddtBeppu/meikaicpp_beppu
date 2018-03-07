1d6c1b96a3a42fb8a5c712cbfe90351484b25f95
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-07 20:47:46 +0900

    e03_12 ご指摘のあった箇所を修正しました。

--- e03_12/src/e03_12.cpp
+++ e03_12/src/e03_12.cpp
@@ -13,24 +13,26 @@
 using namespace std;
 
 int main() {
-	int i_n = 0; // nの値。
+	int i_max = 0; // nの値。この値までの数を足し合わせる
 	int i_sum = 0; // 1からnまでの和を格納する変数
 
 	// nの値の入力を促す
 	cout << "nの値: ";
 	// キーボードからの読み込み
-	cin >> i_n;
+	cin >> i_max;
 
-	for (int i = 1; i <= i_n; i++) {
+	for (int i = 1; i < i_max; i++) {
 		// 1を1ずつ加算し、nの値と等しくなるまで繰り返す
 		cout << i;
-		if (i < i_n) { // 足し合わせる最後の値以外の場合
-			// +を表示
-			cout << " + ";
-		}
+		// +を表示
+		cout << " + ";
 		// 合計値にiの値を加算していく
 		i_sum += i;
 	}
+	// 最後に加算する値を表示
+	cout << i_max;
+	// 最後にnの値を加算
+	i_sum += i_max;
 
 	// 最後に"=和"を表示
 	cout << " = " << i_sum << endl;
