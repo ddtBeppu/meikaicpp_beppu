c41ba07cba65659c93b2b6daff2c023be02a94e5
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-13 20:56:36 +0900

    e03_15  命名規則を見直しました。

--- e03_15/src/e03_15.cpp
+++ e03_15/src/e03_15.cpp
@@ -14,24 +14,26 @@
 using namespace std;
 
 int main() {
-	int i_num = 0; // 整数値を格納する変数。この数の約数を求める。
-	int div = 0; // 整数値を割る数を格納する変数
-	int count = 0; // 約数の数をカウントして格納する変数
+	int i_input = 0; // 整数値を格納する変数。この数の約数を求める。
+	int i_div = 0; // 整数値を割る数を格納する変数
+	int i_count = 0; // 約数の数をカウントして格納する変数
 
-	cout << "整数値："; // 整数値の入力を促す
-	cin >> i_num; // キーボードから整数値を読み込む
+	// 整数値の入力を促す
+	cout << "整数値：";
+	// キーボードから整数値を読み込む
+	cin >> i_input;
 
-	for (div = 1; div <= i_num; div++) {
+	for (i_div = 1; i_div <= i_input; i_div++) {
 		// 整数値を割る数を増加させながら処理を繰り返す
-		if (i_num % div == 0) { // 商が0。割り切れる時
+		if (i_input % i_div == 0) { // 商が0。割り切れる時
 			// 整数を割った数が約数となる。これを表示。
-			cout << div << endl;
+			cout << i_div << endl;
 			// 約数の数を1増加させる
-			count++;
+			i_count++;
 		}
 	}
 	// 約数の総数を表示する
-	cout << "約数は" << count << "個です。" << endl;
+	cout << "約数は" << i_count << "個です。" << endl;
 
 	// 整数値を返す
 	return 0;
