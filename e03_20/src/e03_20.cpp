de7fde2a7852bbee9cee6af0abab091b2dd74ab0
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-13 21:48:23 +0900

    e03_20  空白を出力する関数を作成し、コードの簡略化を行いました。

--- e03_20/src/e03_20.cpp
+++ e03_20/src/e03_20.cpp
@@ -16,21 +16,22 @@ using namespace std;
 void top_left(int length); // 直角が左上側の直角三角形を表示する関数
 void top_right(int length); // 直角が右上側の直角三角形を表示する関数
 void bottom_right(int length); // 直角が右下側の直角三角形を表示する関数
+void take_space(int iter); // 引数の数だけスペースを出力する関数
 
-const string blank = " "; // 1文字分のスペース
+const string s_blank = " "; // 1文字分のスペース
 
 int main() {
 	int i_pos = 0; // 直角の位置を表す整数値を格納する変数
-	string shape[3] = {"左上", "右上", "右下"}; // 直角の位置を表す文字列を格納した配列
+	string s_shape[3] = {"左上", "右上", "右下"}; // 直角の位置を表す文字列を格納した配列
 
 	int i_length = 0; // 段数を表す整数値を格納する変数
-	string retry = "No"; // 続行の是非を示す変数
+	string s_retry = "No"; // 続行の是非を示す変数
 
 	bool first_q = true; // 直角の位置を尋ねるのが初めてならtrue。
 	                     //それ以外falseとして使用する。
 
 	do { // 続行の指示がある間繰り返すループ
-		if (retry != "No"){ // retryがNoでない。すなわち２回目。
+		if (s_retry != "No"){ // retryがNoでない。すなわち２回目。
 			// 処理が継続することを伝える
 			cout << "--続行!!!--" << endl;
 		}
@@ -50,7 +51,7 @@ int main() {
 		} while (i_pos < 0 || i_pos > 2); // 入力値が0,1,2のどれでも無ければ繰り返し
 
 		// 三角形の形状をアナウンス
-		cout << shape[i_pos] << "直角の三角形を表示します。\n";
+		cout << s_shape[i_pos] << "直角の三角形を表示します。\n";
 		// 三角形の段数を尋ねる
 		cout << "段数は：";
 		// キーボードから段数を入力
@@ -58,21 +59,27 @@ int main() {
 
 		switch (i_pos) { // 直角の位置に従い、以下の関数を使い分ける
 		case 0: // 0が入力された時
-			top_left(i_length); // 直角が左上側の直角三角形を表示
-			break; // 処理終了
+			// 直角が左上側の直角三角形を表示
+			top_left(i_length);
+			// 処理終了
+			break;
 		case 1: // 1が入力された時
-			top_right(i_length); // 直角が右上側の直角三角形を表示
-			break; // 処理終了
+			// 直角が右上側の直角三角形を表示
+			top_right(i_length);
+			// 処理終了
+			break;
 		case 2: // 2が入力された時
-			bottom_right(i_length); // 直角が右下側の直角三角形を表示
-			break; // 処理終了
+			// 直角が右下側の直角三角形を表示
+			bottom_right(i_length);
+			// 処理終了
+			break;
 		}
 
 		// 続行するか否かを尋ねる
 		cout << "Retry?  Y/N: ";
 		// キーボードから値を入力
-		cin >> retry;
-	} while (retry == "Y" || retry == "y"); // 続行するとした場合、繰り返し
+		cin >> s_retry;
+	} while (s_retry == "Y" || s_retry == "y"); // 続行するとした場合、繰り返し
 
 	// ループを抜けたので、終了と伝える
 	cout << "--終了!--" << endl;
@@ -83,10 +90,10 @@ int main() {
 
 // 直角が左上側の直角三角形を表示する関数
 void top_left(int length) {
+	// 図形を縦方向に描画
 	for (int cols = 0; cols < length; cols++) {
-		// 図形を縦方向に描画
+		// 直角三角形のため。行の数だけ繰り返し
 		for (int rows = 0; rows < length-cols; rows++) {
-			// 直角三角形のため。行の数だけ繰り返し
 			// 記号を出力して、図形を形成
 			cout << "*";
 		}
@@ -97,18 +104,17 @@ void top_left(int length) {
 
 // 直角が右上側の直角三角形を表示する関数
 void top_right(int length) {
+	// 図形を縦方向に描画
 	for (int cols = 0; cols < length; cols++) {
-		// 図形を縦方向に描画
-		for (int rows = 0; rows < length; rows++) {
-			// 直角三角形のため。行の数だけ繰り返し
-			if (cols - rows > 0) { // 図形の下に行くにつれ、幅が小さくなる
-				// スペースを出力
-				cout << blank;
-			} else {
-				// 記号を出力して、図形を形成
-				cout << "*";
-			}
+		// スペースを出力
+		take_space(cols);
+
+		// 直角三角形のため。行の数だけ繰り返し
+		for (int rows = cols; rows < length; rows++) {
+			// 記号を出力して図形を形成
+			cout << "*";
 		}
+
 		// 改行
 		cout << "\n";
 	}
@@ -116,19 +122,25 @@ void top_right(int length) {
 
 // 直角が右下側の直角三角形を表示する関数
 void bottom_right(int length) {
+	// 図形を縦方向に描画
 	for (int cols = 1; cols <= length; cols++) {
-		// 図形を縦方向に描画
-		for (int rows = 0; rows < length; rows++) {
-			// 直角三角形のため。行の数だけ繰り返し
-			if ((length - cols) - rows > 0) { // 図形の下に行くにつれ、幅を大きくしていく
-				// スペースを出力
-				cout << blank;
-			} else {
-				// 記号を出力して、図形を形成
-				cout << "*";
-			}
+		// スペースを出力
+		take_space(length - cols);
+		// 直角三角形のため。行の数だけ繰り返し
+		for (int rows = 0; rows < cols; rows++) {
+			// 記号を出力して、図形を形成
+			cout << "*";
 		}
 		// 改行
 		cout << "\n";
+	}
+}
+
+// 引数の数だけスペースを出力する関数
+void take_space(int i_count) {
+	// 引数の数だけ繰り返す
+	for (int iter = 0; iter < i_count; iter++) {
+		// スペースを出力
+		cout << s_blank;
 	}
 }
