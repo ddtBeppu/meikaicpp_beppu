b05bb429c8bc38ca42aaa9ced0052ee4cf83b666
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-23 19:05:59 +0900

    e05_03  修正しました。

--- e05_03/src/e05_03.cpp
+++ e05_03/src/e05_03.cpp
@@ -16,6 +16,7 @@ using namespace std;
 int main() {
 	const int sizeArray = 6; // 配列の要素数を定義
 	int iArray[sizeArray] = {}; // 要素数がsizeArrayの配列
+	int iRand = 0; // 乱数を格納する変数
 
 	// 乱数の種を生成
 	srand(time(NULL));
@@ -23,7 +24,7 @@ int main() {
 	// 配列の要素数だけ繰り返し
 	for (int i = 0; i < sizeArray; i++) {
 		// 1 ~ 10の乱数を生成
-		int iRand = rand() % 10 + 1;
+		iRand = rand() % 10 + 1;
 		// 生成した値を要素として代入
 		iArray[i] = iRand;
 	}
