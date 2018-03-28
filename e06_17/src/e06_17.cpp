0926ef78b5ae795c0ae9ff6ebf947f8d4b42107a
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-28 19:17:04 +0900

    e06_17  解答しました。

---
+++ e06_17/src/e06_17.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : e06_17.cpp
+// Author      : Naoto Beppu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 演習6-17
+//               呼び出された回数を表示する関数put_countを作成せよ。
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+const int iNumCall = 10; // 関数を呼び出す回数
+int iCount = 0; // 関数を呼び出すごとに、この値を加算する
+void put_count(); // 呼び出された回数を表示する関数
+
+int main() {
+	// 指定した回数だけ、関数を呼び出す
+	for (int i = 0; i < iNumCall; i++) {
+		// 関数を呼び出して、カウントアップする
+		put_count();
+		// 関数が呼び出された回数を表示する
+		cout << "put_count: " << iCount << "回目" << endl;
+	}
+
+	// 正常終了
+	return 0;
+}
+
+// 呼び出された回数を表示する関数
+void put_count() {
+	// 静的記憶域期間にある変数をカウントアップする
+	iCount++;
+}
