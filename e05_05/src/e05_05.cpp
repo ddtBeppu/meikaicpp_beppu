8be473e68aa790cb96bc966a275f3ab0cd3bc194
Naoto Beppu <ddt.naoto@gmail.com>
2018-03-30 20:10:19 +0900

    e05_05   フラグを用いずに値の重複をなくすように修正を行いました。

--- e05_05/src/e05_05.cpp
+++ e05_05/src/e05_05.cpp
@@ -12,38 +12,36 @@
 using namespace std;
 
 int main() {
-	const int sizeArray = 6; // 配列の要素数を定義
-	int iArray[sizeArray] = {0}; // 要素数がsizeArrayの配列
+	const int iSizeArray = 6; // 配列の要素数を定義
+	int iArray[iSizeArray] = {0}; // 要素数がsizeArrayの配列
 	int iRand = 0; // 乱数を格納する変数
-	bool bCheck = true; // 重複する値が存在するかどうかを判定する変数
 
 	// 乱数の種を生成
 	srand(time(NULL));
 
-	// 配列の要素数だけ繰り返し
-	for (int i = 0; i < sizeArray; i++) {
-		do {
-			// 重複を判定する変数を初期化
-			bCheck = true;
-			// 1 ~ 10の乱数を生成
-			iRand = rand() % 10 + 1;
+	// 配列の最初の要素は先に入れておき、値の重複の確認がやりやすいようにする。
+	iArray[0] = rand() % 10 + 1;
 
-			// 現在、0以外の値が入っている大きさだけ繰り返し
-			for (int n = 0; n <= i; n++) {
-				// 値が重複する場合
-				if (iRand == iArray[n]) {
-					// 判定をfalseとする
-					bCheck = false;
-				}
+	// 配列の要素数だけ繰り返し
+	for (int i = 1; i < iSizeArray; i++) {
+		// 1 ~ 10の乱数を生成
+		iRand = rand() % 10 + 1;
+
+		// n(=i-1)回ループを回し、重複した値がないかを調べる
+		for (int n = 0; n < i; n++) {
+			// 重複した値があった場合には、乱数を再度生成する。
+			if (iRand == iArray[n]) {
+				// 1 ~ 10の乱数を生成
+				iRand = rand() % 10 + 1;
 			}
-		} while (bCheck == false); //重複があればやり直し
+		}
 
 		// 生成した値を要素として代入
 		iArray[i] = iRand;
 	}
 
 	// 配列の要素数だけ繰り返し
-	for (int i = 0; i < sizeArray; i++) {
+	for (int i = 0; i < iSizeArray; i++) {
 		// 配列の要素を出力
 		cout << "iArray[" << i << "]" << " = " << iArray[i] << endl;
 	}
