cf5e4564d3ba58d90898f476caaf856f654a7bf1
Naoto Beppu <ddt.naoto@gmail.com>
2018-05-01 15:00:54 +0900

    e02_11  コメント等の修正を行いました。

--- e02_11/src/e02_11.cpp
+++ e02_11/src/e02_11.cpp
@@ -3,7 +3,7 @@
 // Author      : Naoto Beppu
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Hello World in iInput[2]++, Ansi-style
 //============================================================================
 
 // 演習2-11
@@ -13,38 +13,58 @@
 using namespace std;
 
 int main() {
-	int a, b, c; // 三つの整数値を宣言
-	int max, min, mid; // 最大値、最小値、中央値
-	int num[3]; // 3変数を格納する配列
+	int iInput[3] = {0}; // 3変数を格納する配列
+	int iMax = 0; // 最大値を格納する変数
+	int iMin = 0; // 最小値を格納する変数
+	int iMid = 0; // 中央値を格納する変数
 
-	cout << "a: "; // テキストの出力
-	cin >> a; // キーボードからの入力
+	cout << "三つの整数の中央値を求めます。" << endl;
+	// 一つ目の値
+	cout << "値1: ";
+	// 値の読み込み
+	cin >> iInput[0];
 
-	cout << "b: "; // テキストの出力
-	cin >> b; // キーボードからの入力
+	// 一つ目の値
+	cout << "値2: ";
+	// 値の読み込み
+	cin >> iInput[1];
 
-	cout << "c: "; // テキストの出力
-	cin >> c; // キーボードからの入力
+	// 一つ目の値
+	cout << "値3: ";
+	// 値の読み込み
+	cin >> iInput[2];
 
-	num[0] = a; // 変数を配列に格納
-	num[1] = b; // 変数を配列に格納
-	num[2] = c; // 変数を配列に格納
-
-	max = a; // aを最大値とする
-	if (b > max) max = b; // bがmaxより大きい場合、bを最大値とする
-	if (c > max) max = c; // cがmaxより大きい場合、cを最大値とする
-
-	min = a; // aを最小値とする
-	if (b < min) min = b; // bがminより小さい場合、bを最小値とする
-	if (c < min) min = c; // cがminより小さい場合、cを最小値とする
-
-	for (int i = 0; i < 3; i++){ // ループを回す
-		if (num[i] != max && num[i] != min) // num[i]が最大値でなく、かつ最小値でない場合
-			mid = num[i]; // num[i]を中央値とする
+	// 一つ目の入力値を最大値とし、比較を開始
+	iMax = iInput[0];
+	if (iInput[1] > iMax) { // 2つ目の値が最大値より大きい場合
+		// bを最大値とする
+		iMax = iInput[1];
+	}
+	if (iInput[2] > iMax) { // 3つ目の値が最大値より大きい場合
+		// cを最大値とする
+		iMax = iInput[2];
 	}
 
-	// 結果出力
-	cout << "中央値は" << mid << "です。\n"<< endl;
+	// 一つ目の入力値を最小値とし、比較を開始
+	iMin = iInput[0];
+	if (iInput[1] < iMin) { // 2つ目の値が最小値より小さい場合
+		// bを最小値とする
+		iMin = iInput[1];
+	}
+	if (iInput[2] < iMin) { // 3つ目の値が最小値より小さい場合
+		// cを最小値とする
+		iMin = iInput[2];
+	}
 
-	return 0; // 整数を返す
+	for (int i = 0; i < 3; i++){ // 3つの値に順にアクセス
+		if (iInput[i] != iMax && iInput[i] != iMin) { // 現在の最大値でなく、かつ最小値でない場合
+			// 現在の値を中央値とする
+			iMid = iInput[i];
+		}
+	}
+	// 中央値を表示する
+	cout << "中央値は" << iMid << "です。\n"<< endl;
+
+	// 正常終了
+	return 0;
 }
